e8f65b1846a2403fa33ad38a6b720550e081e138
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-07 15:08:40 -0500

    Create 22_23.js

---
+++ 22_23.js
@@ -0,0 +1,21 @@
+fetch('2022_23_final.csv')
+    .then(response => response.text())
+    .then(data => {
+        const rows = data.trim().split('\n').slice(1); // Skip the header row
+
+        const tableBody = document.getElementById('22_23');
+
+        rows.forEach(row => {
+            const columns = row.split(',');
+
+            const tr = document.createElement('tr');
+
+            columns.forEach(column => {
+                const td = document.createElement('td');
+                td.textContent = column;
+                tr.appendChild(td);
+            });
+
+            tableBody.appendChild(tr);
+        });
+    });
