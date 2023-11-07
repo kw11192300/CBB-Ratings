ed1ed8482feea7b14ae9cd340aa0d72744abd4b7
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-07 13:09:33 -0500

    Create All_Games.js

---
+++ All_Games.js
@@ -0,0 +1,21 @@
+fetch('All_Games.csv')
+    .then(response => response.text())
+    .then(data => {
+        const rows = data.trim().split('\n').slice(1); // Skip the header row
+
+        const tableBody = document.getElementById('gameData');
+
+        rows.forEach(row => {
+            const columns = row.split(',');
+
+            const tr = document.createElement('tr');
+
+            columns.forEach(column => {
+                const td = document.createElement('td');
+                td.textContent = column;
+                tr.appendChild(td);
+            });
+
+            tableBody.appendChild(tr);
+        });
+    });
