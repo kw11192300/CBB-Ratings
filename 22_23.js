209474d253e8de08c7c0c1020c7ebf1f70439bdf
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-07 15:28:03 -0500

    Update 22_23.js

--- 22_23.js
+++ 22_23.js
@@ -1,9 +1,9 @@
-fetch('2022_23_Final.csv')
+fetch('2022_23_Final2.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.trim().split('\n').slice(1); // Skip the header row
 
-        const tableBody = document.getElementById('22_23');
+        const tableBody = document.getElementById('22_232');
 
         rows.forEach(row => {
             const columns = row.split(',');
