2032730eb22229804323fab770920bba904616fb
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-08 11:23:27 -0500

    Update and rename script2.js to script3.js

--- script3.js
+++ script3.js
@@ -1,6 +1,6 @@
 // Function to create and populate a table
 function createTable(data) {
-    const tableContainer = document.getElementById("table-container2");
+    const tableContainer = document.getElementById("table-container3");
     const table = document.createElement("table");
     const headers = ["Change","Rank", "Team", "Conf", "W-L", "Rating", "Win Probability"];
 
@@ -28,8 +28,8 @@ function createTable(data) {
     tableContainer.appendChild(table);
 }
 
-// Fetch data from the CSV file (CBB2.csv)
-fetch("CBB2.csv")
+// Fetch data from the CSV file (CBB3.csv)
+fetch("CBB3.csv")
     .then(response => response.text())
     .then(data => {
         const rows = data.split("\n");
