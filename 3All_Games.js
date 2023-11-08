2da0264a9517e177f39f71b4453d0c09fcd2e49c
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-08 11:44:07 -0500

    Update and rename 2All_Games.js to 3All_Games.js

--- 3All_Games.js
+++ 3All_Games.js
@@ -1,9 +1,9 @@
-fetch('2All_Games_2023.csv')
+fetch('3All_Games_2023.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.trim().split('\n').slice(1); // Skip the header row
 
-        const tableBody = document.getElementById('gameData2');
+        const tableBody = document.getElementById('gameData3');
 
         rows.forEach(row => {
             const columns = row.split(',');
