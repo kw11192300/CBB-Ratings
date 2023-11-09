7cea0240a00fbb84007626e143074c7fc9e27b97
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-09 11:48:45 -0500

    Update and rename top_picks6.js to top_picks7.js

--- top_picks7.js
+++ top_picks7.js
@@ -1,9 +1,9 @@
-fetch('6Model_Best_Bets_2023.csv')
+fetch('7Model_Best_Bets_2023.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.trim().split('\n').slice(1); // Skip the header row
 
-        const tableBody = document.getElementById('toppicks6');
+        const tableBody = document.getElementById('toppicks7');
 
         rows.forEach(row => {
             const columns = row.split(',');
