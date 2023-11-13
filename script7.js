3ef8df40912e344c7daab311db5e4548042ca838
Kyle Wasilewski <106460168+kw11192300@users.noreply.github.com>
2023-11-13 11:30:25 -0500

    Update and rename script6.js to script7.js

--- script7.js
+++ script7.js
@@ -1,6 +1,6 @@
 // Function to create and populate a table
 function createTable(data) {
-    const tableContainer = document.getElementById("table-container6");
+    const tableContainer = document.getElementById("table-container7");
     const table = document.createElement("table");
     const headers = ["Change","Rank", "Team", "Conf", "W-L", "Rating"];
 
@@ -28,8 +28,8 @@ function createTable(data) {
     tableContainer.appendChild(table);
 }
 
-// Fetch data from the CSV file (CBB6.csv)
-fetch("CBB6.csv")
+// Fetch data from the CSV file (CBB7.csv)
+fetch("CBB7.csv")
     .then(response => response.text())
     .then(data => {
         const rows = data.split("\n");
