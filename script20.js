981cc94cbd2dc20c68e4f41428be96d7c1e5fd1a
theBAM <106460168+thebasketballanalyticsmodel@users.noreply.github.com>
2023-11-21 09:51:07 -0500

    Update and rename script19.js to script20.js

--- script20.js
+++ script20.js
@@ -1,6 +1,6 @@
 // Function to create and populate a table
 function createTable(data) {
-    const tableContainer = document.getElementById("table-container19");
+    const tableContainer = document.getElementById("table-container20");
     const table = document.createElement("table");
     const headers = ["Change","Rank", "Team", "Conf", "W-L", "Rating"];
 
@@ -28,8 +28,8 @@ function createTable(data) {
     tableContainer.appendChild(table);
 }
 
-// Fetch data from the CSV file (CBB19.csv)
-fetch("CBB19.csv")
+// Fetch data from the CSV file (CBB20.csv)
+fetch("CBB20.csv")
     .then(response => response.text())
     .then(data => {
         const rows = data.split("\n");
