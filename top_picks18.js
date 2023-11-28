3a99ab12373cd91b1f8de35e8601f3a013240ee8
theBAM <106460168+thebasketballanalyticsmodel@users.noreply.github.com>
2023-11-28 18:56:12 -0500

    Update and rename top_picks17.js to top_picks18.js

--- top_picks18.js
+++ top_picks18.js
@@ -1,9 +1,9 @@
-fetch('17Model_Best_Bets_2023.csv')
+fetch('18Model_Best_Bets_2023.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.trim().split('\n').slice(1); // Skip the header row
 
-        const tableBody = document.getElementById('toppicks17');
+        const tableBody = document.getElementById('toppicks18');
 
         rows.forEach(row => {
             const columns = row.split(',');
