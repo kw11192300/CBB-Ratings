ca4721ab5770683532060389224ada9282315c6e
theBAM <106460168+thebasketballanalyticsmodel@users.noreply.github.com>
2023-11-30 17:38:36 -0500

    Update and rename top_picks19.js to top_picks20.js

--- top_picks20.js
+++ top_picks20.js
@@ -1,9 +1,9 @@
-fetch('19Model_Best_Bets_2023.csv')
+fetch('20Model_Best_Bets_2023.csv')
     .then(response => response.text())
     .then(data => {
         const rows = data.trim().split('\n').slice(1); // Skip the header row
 
-        const tableBody = document.getElementById('toppicks19');
+        const tableBody = document.getElementById('toppicks20');
 
         rows.forEach(row => {
             const columns = row.split(',');
